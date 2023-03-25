d0c24fa5ef59a77866b1e5bae1f4c9bb81154d1b
pgolanowska <paulina.golanowska@gmail.com>
2023-03-25 10:26:37 +0100

    Add project files.

---
+++ VetClinic.Intranet/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace VetClinic.Intranet.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
